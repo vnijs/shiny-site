89831bcf6f7574f9a3ef164d0b45edbcf0079eda
mostly-harmless <vnijs@ucsd.edu>
2015-06-17 09:18:20 -0700

    Removing html_preserve generated by Knitr

--- global.R
+++ global.R
@@ -6,7 +6,7 @@ inclRmd <- function(path, r_env = parent.frame()) {
   paste(readLines(path, warn = FALSE), collapse = '\n') %>%
   knitr::knit2html(text = ., fragment.only = TRUE, envir = r_env,  options = "",
                    stylesheet = "") %>%
-    gsub("&lt;!--/html_preserve--&gt;","",.) %>%
-    gsub("&lt;!--html_preserve--&gt;","",.) %>%
+    gsub("&lt;!--/html_preserve--&gt;","",.) %>%  ## knitr adds this
+    gsub("&lt;!--html_preserve--&gt;","",.) %>%   ## knitr adds this
     HTML
 }
