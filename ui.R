1e6aec04a5182614e7ebeaed36ca230c78b586e7
vnijs <radiant@rady.ucsd.edu>
2016-06-07 12:06:12 -0700

    Fix for widgets suggested by @timelyportfolio https://github.com/timelyportfolio/functionplotR/issues/1#issuecomment-224369431

--- ui.R
+++ ui.R
@@ -1,7 +1,20 @@
+## Thanks for @timelyportfolio for this comment/fix
+## https://github.com/timelyportfolio/functionplotR/issues/1#issuecomment-224369431
+getdeps <- function() {
+  htmltools::attachDependencies(
+    htmltools::tagList(),
+    c(
+      htmlwidgets:::getDependency("functionplot","functionplotR"),
+      htmlwidgets:::getDependency("datatables","DT"),
+      recursive = FALSE
+    )
+  )
+}
+
 shinyUI(
   navbarPage("Shiny-site",
     tabPanel("Page 1", uiOutput("page1")),
     tabPanel("Page 2", uiOutput("page2")),
-    tabPanel("Page 3", uiOutput("page3"))
+    tabPanel("Page 3", uiOutput("page3"), getdeps())
   )
 )
