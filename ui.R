d88e860b6ead5081b427bd0e6040c761b77fb648
Brandon Bertelsen <brandon.bertelsen@credoconsulting.ca>
2016-06-27 00:57:06 -0400

    Updates for shinydashboard

--- ui.R
+++ ui.R
@@ -5,8 +5,7 @@ getdeps <- function() {
     htmltools::tagList(),
     c(
       htmlwidgets:::getDependency("functionplot","functionplotR"),
-      htmlwidgets:::getDependency("datatables","DT"),
-      recursive = FALSE
+      htmlwidgets:::getDependency("datatables","DT")
     )
   )
 }
@@ -18,17 +17,21 @@ sidebar <-
     menuItem("Page 3",tabName = "page3")
   )
 
+body <-
+  tabItems(
+    tabItem(tabName = "page1", uiOutput("page1")),
+    tabItem(tabName = "page2", uiOutput("page2")),
+    tabItem(tabName = "page3", uiOutput("page3"))
+  )
+
 dashboardPage(
-    dashboardHeader(title = "Hello"),
+    dashboardHeader(title = "Title"),
     dashboardSidebar(
       sidebar
     ),
     dashboardBody(
-      tabItems(
-        tabItem(tabName = "page1", uiOutput("page1")),
-        tabItem(tabName = "page2", uiOutput("page2")),
-        tabItem(tabName = "page3", uiOutput("page3"))
-      )
-    )
+      body
+    ),
+    getdeps()
 )
 
